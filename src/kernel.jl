e15d56ea09fbdf40b57312de1490a39594c4d6d0
Chris Geoga <geoga@wisc.edu>
2026-06-25 13:33:09 -0500

    Add safety checks in lattice constructor

--- src/kernel.jl
+++ src/kernel.jl
@@ -7,62 +7,79 @@ end
 
 gen_preconditioner(::NoPreconditioner, pts, kernel) = I
 
-function lattice_kernel_matrix(pts::Vector{Float64}, kernel, 
-                               preconditioner=NoPreconditioner())
+function identify_lattice(pts::Vector{Float64})
   t_min = minimum(pts)
   t_max = maximum(pts)
   dt    = minimum(diff(pts))
   N     = round(Int, (t_max - t_min)/dt) + 1
-  ixs   = round.(Int, (pts .- t_min)./dt) .+ 1
-  v     = [kernel(d * dt) for d in 0:(N-1)]
-  toep  = SymToeplitz(v)
-  buf1  = zeros(Float64, N)
-  buf2  = zeros(Float64, N)
-  pre   = gen_preconditioner(preconditioner, pts, kernel)
-  MaskedSymToeplitz(toep, ixs, buf1, buf2, pre)
+  if N > 10 * length(pts)
+    @warn "Embedding these points in a grid requires an internal FFT of size $(2*N-1). If this is larger than expected, please double check your provided locations."
+  end
+  ixs  = round.(Int, (pts .- t_min)./dt) .+ 1
+  grid = [d*dt for d in 0:(N-1)]
+  (;ixs, grid)
 end
 
-function lattice_kernel_matrix(pts1::Vector{Float64}, pts2::Vector{Float64}, kernel)
+function lattice_kernel_matrix(pts::Vector{Float64}, kernel, 
+                               preconditioner=NoPreconditioner())
+  (;ixs, grid) = identify_lattice(pts)
+  toep = SymToeplitz(kernel.(grid))
+  pre  = gen_preconditioner(preconditioner, pts, kernel)
+  MaskedSymToeplitz(toep, ixs, zeros(length(grid)), zeros(length(grid)), pre)
+end
+
+function identify_lattice(pts1::Vector{Float64}, pts2::Vector{Float64})
   all_pts = sort(unique(vcat(pts1, pts2)))
   t_min = all_pts[1]
   t_max = all_pts[end]
   dt = length(all_pts) > 1 ? minimum(diff(all_pts)) : 1.0
   N  = round(Int, (t_max - t_min) / dt) + 1
+  if N > 10 * length(all_pts)
+    @warn "Embedding these points in a grid requires an internal FFT of size $(2*N-1). If this is larger than expected, please double check your provided locations."
+  end
   ixs_out = round.(Int, (pts1 .- t_min) ./ dt) .+ 1
   ixs_in  = round.(Int, (pts2 .- t_min) ./ dt) .+ 1
-  toep = SymToeplitz([kernel(d * dt) for d in 0:(N-1)])
-  buf1 = zeros(Float64, N)
-  buf2 = zeros(Float64, N)
-  CrossMaskedSymToeplitz(toep, ixs_out, ixs_in, buf1, buf2)
+  grid    = [d*dt for d in 0:(N-1)]
+  (;N, ixs_in, ixs_out, grid)
 end
 
-function lattice_kernel_matrix(pts::Vector{SVector{2,Float64}}, kernel,
-                               preconditioner=NoPreconditioner())
+function lattice_kernel_matrix(pts1::Vector{Float64}, pts2::Vector{Float64}, kernel)
+  (;N, ixs_in, ixs_out, grid) = identify_lattice(pts1, pts2)
+  toep = SymToeplitz(kernel.(grid))
+  CrossMaskedSymToeplitz(toep, ixs_out, ixs_in, 
+                         zeros(length(grid)), zeros(length(grid)))
+end
 
+function identify_lattice(pts::Vector{SVector{2,Float64}})
   ex1 = extrema(x->x[1], pts)
   ex2 = extrema(x->x[2], pts)
   dx1 = minimum(diff(sort(unique(getindex.(pts, 1)))))
   dx2 = minimum(diff(sort(unique(getindex.(pts, 2)))))
   nx  = round(Int, (ex1[2] - ex1[1])/dx1) + 1
   ny  = round(Int, (ex2[2] - ex2[1])/dx2) + 1
+  if (nx * ny) > 10 * length(pts)
+    @warn "Embedding these points in a grid requires an internal FFT of size $(2*nx-1) x $(2*ny-1). If this is larger than expected, please double check your provided locations."
+  end
   lag1r = range(0.0, step=dx1, length=nx)
   lag2r = range(0.0, step=dx2, length=ny)
-  full_columns = map(lag1r) do l1
-    [kernel(SVector(l1, l2)) for l2 in lag2r]
-  end
   observed_ixs = map(pts) do pt
     i = round(Int, (pt[2] - ex2[1])/ dx2) + 1
     j = round(Int, (pt[1] - ex1[1])/ dx1) + 1
     i + (j - 1)*ny
   end
-  pre   = gen_preconditioner(preconditioner, pts, kernel)
+  (;lag1r, lag2r, observed_ixs)
+end
+
+function lattice_kernel_matrix(pts::Vector{SVector{2,Float64}}, kernel,
+                               preconditioner=NoPreconditioner())
+  (;lag1r, lag2r, observed_ixs) = identify_lattice(pts)
+  full_columns = [[kernel(SVector(l1, l2)) for l2 in lag2r] for l1 in lag1r]
+  pre = gen_preconditioner(preconditioner, pts, kernel)
   MaskedSymBTTB(full_columns, observed_ixs, pre)
 end
 
-function lattice_kernel_matrix(pts1::Vector{SVector{2,Float64}}, 
-                               pts2::Vector{SVector{2,Float64}}, 
-                               kernel)
-  # Combine to find the global grid boundaries and resolution
+function identify_lattice(pts1::Vector{SVector{2,Float64}}, 
+                          pts2::Vector{SVector{2,Float64}})
   all_pts = vcat(pts1, pts2)
   ex1 = extrema(x->x[1], all_pts)
   ex2 = extrema(x->x[2], all_pts)
@@ -70,11 +87,11 @@ function lattice_kernel_matrix(pts1::Vector{SVector{2,Float64}},
   dx2 = minimum(diff(sort(unique(getindex.(all_pts, 2)))))
   nx  = round(Int, (ex1[2] - ex1[1])/dx1) + 1
   ny  = round(Int, (ex2[2] - ex2[1])/dx2) + 1
+  if (nx * ny) > 10 * length(all_pts)
+    @warn "Embedding these points in a grid requires an internal FFT of size $(2*nx-1) x $(2*ny-1). If this is larger than expected, please double check your provided locations."
+  end
   lag1r = range(0.0, step=dx1, length=nx)
   lag2r = range(0.0, step=dx2, length=ny)
-  full_columns = map(lag1r) do l1
-    [kernel(SVector(l1, l2)) for l2 in lag2r]
-  end
   ixs_out = map(pts1) do pt
     i = round(Int, (pt[2] - ex2[1])/dx2) + 1
     j = round(Int, (pt[1] - ex1[1])/dx1) + 1
@@ -85,9 +102,19 @@ function lattice_kernel_matrix(pts1::Vector{SVector{2,Float64}},
     j = round(Int, (pt[1] - ex1[1])/dx1) + 1
     i + (j-1)*ny
   end
+  (;lag1r, lag2r, ixs_in, ixs_out)
+end
+
+function lattice_kernel_matrix(pts1::Vector{SVector{2,Float64}}, 
+                               pts2::Vector{SVector{2,Float64}}, 
+                               kernel)
+  (;lag1r, lag2r, ixs_in, ixs_out) = identify_lattice(pts1, pts2)
+  full_columns = map(lag1r) do l1
+    [kernel(SVector(l1, l2)) for l2 in lag2r]
+  end
   bttb = SymBTTB(full_columns)
-  buf1 = zeros(Float64, ny*nx)
-  buf2 = zeros(Float64, ny*nx)
+  buf1 = zeros(Float64, length(lag1r)*length(lag2r))
+  buf2 = zeros(Float64, length(lag1r)*length(lag2r))
   CrossMaskedSymBTTB(bttb, ixs_out, ixs_in, buf1, buf2)
 end
 
